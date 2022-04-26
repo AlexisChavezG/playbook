f989012ccbb94814b4d0024ade6d92b5e92da6ec
AlexisChavezG <alexis.chavez94@outlook.com>
2022-04-25 19:59:01 -0500

    facebook2

---
+++ weekly_mission_2/exercises/facebook.js
@@ -0,0 +1,21 @@
+const user = {
+    userName : "Alexis_Chavez",
+    fistName : "Alexis",
+    lastName : "Chávez",
+    location : "Puebla,Mx",
+    age :"27",
+    dateAdd : function(){
+        return new Date()
+    }
+}
+const post = {
+    numberPost : "29",
+    dateLastPost : function(){
+        return new Date()
+    },
+    mePost : "Cultura informatica",
+
+}
+const biography = {
+    
+}
